b38936af7c8ee4fedd99af7222d16100095cef0d
Sahul54 <ksahul9576@gmail.com>
2024-03-31 18:59:00 +0530

    event Listener

---
+++ miniProject/App.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector("button");
+
+// btn.onclick = function () {
+//     // console.log("Button clicked!");
+//     alert("Are you sure!")
+// }
+
+
+// btn.onmouseenter = function () {
+//     // console.log("Button clicked!");
+//     alert("Are you sure!")
+// }
+
+btn.addEventListener("click", function () {
+    console.log("clicked-1");
+    console.log("clicked-2");
+    console.log("clicked-1");
+    console.log("clicked-2");
+})
