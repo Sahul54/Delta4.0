e23e94aca7b14ca47bfc9c7b58b9cb6d268a77fc
Sahul54 <ksahul9576@gmail.com>
2024-04-05 22:29:05 +0530

    async and await

--- 03 JAVA SCRIPT/Part 12/app.js
+++ 03 JAVA SCRIPT/Part 12/app.js
@@ -1,13 +1,30 @@
-async function greet(){
-    // throw "random error";
-       return "Hello!"
-}
+// async function greet(){
+//     // throw "random error";
+//     //    return "Hello!"
+// }
 //  greet();
 
-greet()
-    .then( (result) => {
-        console.log("Promises was accepted");
-    })
-    .catch((err) => {
-        console.log("promise was rectected");
-    });
+// greet()
+//     .then( (result) => {
+//         console.log("Promises was accepted");
+//     })
+//     .catch((err) => {
+//         console.log("promise was rectected");
+//     });
+
+
+function getNum() {
+    return new Promise((resolve, rejrct) => {
+        setTimeout ( ()=>{
+            let num = Math.floor(Math.random() *10)+1;
+            console.log((num));
+        }, 1000)
+    }) 
+}
+
+async function demo(){
+    await getNum();
+    await getNum();
+    await getNum();
+    getNum();
+}
