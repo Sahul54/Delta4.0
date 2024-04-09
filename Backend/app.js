4325983e42376f7c9f8548d8b26cf0779cd3bd36
Sahul54 <ksahul9576@gmail.com>
2024-04-10 03:03:31 +0530

    Node js

---
+++ Backend/app.js
@@ -0,0 +1,23 @@
+// console.log("Namasate!");
+// console.log(process.argv);
+
+// let arg = process.argv;
+// for (let i = 0; i < arg.length; i++) {
+   
+//    console.log("hello  " + arg[i]);
+    
+// }
+
+
+// const PIvalue = require("./math");
+
+// console.log(PIvalue);
+
+
+// const info = require("./Fruits");
+//  console.log(info);
+
+
+import { sum, PI} from "./math.js"
+
+console.log(sum(1,3)); 
---
+++ Backend/math.js
@@ -0,0 +1,14 @@
+// const PI = 3.14;
+
+// let fruits = {
+//     apple : 120,
+//     papaya : 30
+// }
+
+// module.exports = (PI);
+// module.exports = (fruits);
+
+
+export const PI = 3.14;
+
+export const sum = (a,b) => a+b;
