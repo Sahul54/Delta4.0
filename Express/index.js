a4fc4430c6e19e614c144fcea3f17b6b3d401ec1
Sahul54 <ksahul9576@gmail.com>
2024-04-12 03:58:35 +0530

    Express

---
+++ Express/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
+app.get("/", (req, res) =>{
+    res.send("You conntected root path");
+});
+
+app.get("/home", (req, res) =>{
+    res.send("You connected home page");
+});
+
+app.get("*", (req, res) =>{
+    res.send("This page is note exits")
+})
+
+app.post("/post", (req, res) =>{
+    res.send("This page is post page");
+})
+
+// app.use((req, res) =>{
+//     // console.log(req);   // send request
+//     console.log("request recived");
+//     res.send("hello!");
+// });
